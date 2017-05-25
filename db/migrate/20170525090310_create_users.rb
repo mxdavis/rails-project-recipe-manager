76da0ffbb148531a2e1153cfb03243bcd428b520
mxdavis <md01@me.com>
2017-05-25 14:43:17 +0300

    change users password column to password_digest for bcrypt

--- db/migrate/20170525090310_create_users.rb
+++ db/migrate/20170525090310_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :password, null: false
+      t.string :password_digest, null: false
       t.boolean :admin, default: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -59,11 +59,11 @@ ActiveRecord::Schema.define(version: 20170525094511) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                       null: false
-    t.string   "password",                   null: false
-    t.boolean  "admin",      default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name",                            null: false
+    t.string   "password_digest",                 null: false
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
