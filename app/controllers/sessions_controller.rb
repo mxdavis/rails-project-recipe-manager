16a58f36fd807f200708bc365d6dd4a00c473251
mxdavis <md01@me.com>
2017-05-28 22:05:14 +0300

    fix indentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,18 @@
 class SessionsController < ApplicationController
   protect_from_forgery with: :null_session
-    def create
-      if request.env["omniauth.auth"].present?
-        user = User.find_or_initialize_by(name: request.env["omniauth.auth"]['info']['name'])
-        if user.id.present?
-          session[:user_id] = user.id
-          redirect_to user_path(user), notice: "You have successfully logged in"
-        else
-          user.password = SecureRandom.hex
-          user.save
-          session[:user_id] = user.id
-          redirect_to user_path(user), notice: "You have successfully logged in"
-        end
+  def create
+    if request.env["omniauth.auth"].present?
+      user = User.find_or_initialize_by(name: request.env["omniauth.auth"]['info']['name'])
+      if user.id.present?
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "You have successfully logged in"
+      else
+        user.password = SecureRandom.hex
+        user.save
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "You have successfully logged in"
       end
+    end
     
     if params[:name].present?
       user = User.find_by_name(params[:name])
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
           flash[:alert] = "Your password is not correct"
         else 
           flash[:alert] = "The username is invalid"
-        end
+       end
         render 'new'
       end
     end
