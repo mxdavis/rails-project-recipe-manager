f8ff2e75a1f2942fb7b64a7c231df0b520146e2a
mxdavis <md01@me.com>
2017-07-03 23:51:07 +0300

    serialize working for favorites, but need to get nested serializers to work (user and recipe)

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -2,6 +2,4 @@ class FavoriteSerializer < ActiveModel::Serializer
   attributes :id
   belongs_to :user, serializer: UserSerializer
   belongs_to :recipe, serializer: RecipeSerializer
-
-  url [:recipe, :user, :favorite]
 end
