f6f502200b191c58031c8f70fd9c98f7eea9b1bb
mxdavis <md01@me.com>
2017-07-05 01:11:23 +0300

    cleanup old js method

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,19 +1,3 @@
-
-// $(document).on("click", "span.favorited", function(e){
-//   e.preventDefault()
-//   var recipeId = parseInt(e.target.parentNode.dataset.recipe)
-//   $.post(`/recipes/${recipeId}/unfavorite_recipe`)
-//   this.children["0"].innerHTML = '<img src="/assets/heart.png" alt="Heart">'
-//   this.className = "favorite"
-// })
-
-// $(document).on("click", "span.favorite", function(e){
-//   e.preventDefault()
-//   var recipeId = parseInt(e.target.parentNode.dataset.recipe)
-//   $.post(`/recipes/${recipeId}/favorite_recipe`)
-//     this.children["0"].innerHTML = '<img src="/assets/heart-red.png" alt="Heart">'
-//     this.className = "favorited"
-// })
 var f
 var dom
 
