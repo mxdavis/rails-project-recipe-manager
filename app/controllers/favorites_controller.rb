07bb827436935aebbb792fd56ace99020c07f0c4
mxdavis <md01@me.com>
2017-07-05 00:50:59 +0300

    favoritng works! but unfavoriting does not

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,38 +5,50 @@ class FavoritesController < ApplicationController
     render json: favorite
   end
 
-  def favorite
-    if logged_in? 
-      recipe = find_by_recipe_id
-      favorite = Favorite.new(recipe: recipe, user: current_user)
-      if favorite.save 
-        render json: favorite, status: 201
-        flash["notice"] = "You have favorited #{recipe.name}"
-      else 
-        flash["alert"] = "Something has gone wrong"
-      end
-    else 
-      flash["alert"] = "Please login to favorite recipes"
-    end
-    redirect_back(fallback_location: root_path)
-  end
+  # def favorite
+  #   if logged_in? 
+  #     recipe = find_by_recipe_id
+  #     favorite = Favorite.new(recipe: recipe, user: current_user)
+  #     if favorite.save 
+  #       render json: favorite, status: 201
+  #       flash["notice"] = "You have favorited #{recipe.name}"
+  #     else 
+  #       flash["alert"] = "Something has gone wrong"
+  #     end
+  #   else 
+  #     flash["alert"] = "Please login to favorite recipes"
+  #   end
+  #   redirect_back(fallback_location: root_path)
+  # end
 
-  def unfavorite
-    if logged_in? 
-      recipe = find_by_recipe_id
-      favorite = Favorite.find_by(recipe: recipe, user: current_user)
-      favorite.delete
-      respond_to do |format|
-        format.json { render json: favorite }
-      end
-      flash["notice"] = "You have unfavorited #{recipe.name}"
-    else 
-      flash["alert"] = "Please login to favorite recipes"
-    end
-    redirect_back(fallback_location: root_path)
-  end
+  # def unfavorite
+  #   if logged_in? 
+  #     recipe = find_by_recipe_id
+  #     favorite = Favorite.find_by(recipe: recipe, user: current_user)
+  #     favorite.delete
+  #     respond_to do |format|
+  #       format.json { render json: favorite }
+  #     end
+  #     flash["notice"] = "You have unfavorited #{recipe.name}"
+  #   else 
+  #     flash["alert"] = "Please login to favorite recipes"
+  #   end
+  #   redirect_back(fallback_location: root_path)
+  # end
 
   def update
-    binding.pry 
+    if logged_in? 
+      if params["id"] == ""
+        favorite = Favorite.create(recipe: find_by_recipe_id, user: current_user)
+        render json: favorite, status: 201
+      else
+        favorite = Favorite.find_by(recipe: find_by_recipe_id, user: current_user)
+        favorite.delete
+        render json: favorite, status: 201
+      end
+    else 
+      flash["alert"] = "Please login to favorite recipes"
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
