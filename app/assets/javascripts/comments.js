cb96202cc24ce1f302b098a8a50827ed8f9a6ed3
mxdavis <md01@me.com>
2017-07-05 02:00:44 +0300

    create comment class

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,13 @@
+class Comment {
+  constructor(rating, description, recipeId, userId, id) {
+    this.rating = rating
+    this.description = description
+    this.recipeId = recipeId
+    this.userId = userId
+    this.id = id
+  }
+}
+
 $(document).on("submit", "div.comment-form", function(e){
   console.log("I worked!");
   e.preventDefault();
