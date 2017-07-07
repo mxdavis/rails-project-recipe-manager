c954fa576352a7802311258715581b1d20b2acd3
mxdavis <md01@me.com>
2017-07-07 05:10:39 +0300

    update handlebars template name, and get handlebar to load each recipe object

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -2,43 +2,45 @@ var title
 var r
 
 class Recipe {
-  constructor(recipeId, name, timeInMinutes, instructions, userId, favoriteId) {
+  constructor(recipeId, name, timeInMinutes, instructions, userId, favoriteId, template) {
     this.recipeId = recipeId
     this.name = name
     this.timeInMinutes = timeInMinutes
     this.instructions = instructions
     this.userId = userId
     this.favoriteId = favoriteId
+    this.template = template
   }
 }
 
 Recipe.prototype.returnRecipeHtml = function(){
+  debugger
   var source   = $("#recipe-template").html();
   var template = Handlebars.compile(source);
   var context = {
-    recipeId: recipeId,
-    name: name,
-    timeInMinutes: timeInMinutes,
-    instructions: instructions,
-    userId: userId,
-    favoriteId: favoriteId
+    recipeId: r.recipeId,
+    name: r.name,
+    timeInMinutes: r.timeInMinutes,
+    instructions: r.instructions,
+    userId: r.userId,
   };
-
-  
-var createRecipes = recipes => {
-  recipes.data.forEach(function(recipe) {
-    r = new Recipe(recipe.id, recipe.attributes.name, recipe.attributes["time-in-minutes"], recipe.attributes.instructions, recipe.relationships.user.data.id, recipe.relationships.favorites.data)
-    addRecipeToView(r)
-  end
-  })
+  r.template = template(context);
+  $('#list-recipes').append(r.template)
 }
 
+var createRecipes = recipes => {
+  recipes.data.forEach(function(recipe) {
+    r = new Recipe(recipe.id, recipe.attributes.name, String(recipe.attributes["time-in-minutes"]), recipe.attributes.instructions, recipe.relationships.user.data.id, recipe.relationships.favorites.data)
+    r.returnRecipeHtml()
+  })
+}
 $(document).on("click", "#sort-recipes-by", function(e){
-        var url = e.target.href
-        title = e.target.text
-        $.get(url, function(recipes){
-          // $('#list-recipes').html(recipes)
-          createRecipes(recipes)
-          })
-        e.preventDefault()
+  var url = e.target.href
+  title = `<h1>${e.target.text}</h1>`
+  $('#list-recipes').html("")
+  $('#list-recipes').append(title)
+  $.get(url, function(recipes){
+    createRecipes(recipes)
+    })
+  e.preventDefault()
 })
