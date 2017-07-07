ab61dfa396d426f707e6eb47f4a4e6548f8a5eac
mxdavis <md01@me.com>
2017-07-07 04:18:13 +0300

    save each recipe to object, prepare handle bars to receive object

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,24 +1,44 @@
 var title
+var r
 
 class Recipe {
-  constructor(recipeId, userId, id) {
+  constructor(recipeId, name, timeInMinutes, instructions, userId, favoriteId) {
     this.recipeId = recipeId
+    this.name = name
+    this.timeInMinutes = timeInMinutes
+    this.instructions = instructions
     this.userId = userId
-    this.id = id
+    this.favoriteId = favoriteId
   }
 }
 
-$(document).ready(function() {
-    $('#sort-recipes-by').on("click", function (e) {
-      var url = e.target.href
-      title = e.target.text
-      debugger
-      var title
-      $.get(url, function(recipes){
-      //   $('#list-recipes').html(recipes)
-        debugger
+Recipe.prototype.returnRecipeHtml = function(){
+  var source   = $("#recipe-template").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    recipeId: recipeId,
+    name: name,
+    timeInMinutes: timeInMinutes,
+    instructions: instructions,
+    userId: userId,
+    favoriteId: favoriteId
+  };
 
-      })
-      e.preventDefault()
+  
+var createRecipes = recipes => {
+  recipes.data.forEach(function(recipe) {
+    r = new Recipe(recipe.id, recipe.attributes.name, recipe.attributes["time-in-minutes"], recipe.attributes.instructions, recipe.relationships.user.data.id, recipe.relationships.favorites.data)
+    addRecipeToView(r)
+  end
   })
+}
+
+$(document).on("click", "#sort-recipes-by", function(e){
+        var url = e.target.href
+        title = e.target.text
+        $.get(url, function(recipes){
+          // $('#list-recipes').html(recipes)
+          createRecipes(recipes)
+          })
+        e.preventDefault()
 })
