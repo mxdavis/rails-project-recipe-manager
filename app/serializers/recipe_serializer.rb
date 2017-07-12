7bf98e56484a0d952827c0215ba83b9afd15e7e2
mxdavis <md01@me.com>
2017-07-12 04:59:01 +0300

    realign def

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -12,7 +12,7 @@ class RecipeSerializer < ActiveModel::Serializer
     end
   end
 
-    def formatted_comments
+  def formatted_comments
     object.comments.map do |c|
       {
         rating: c.rating,
